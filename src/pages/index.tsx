aadc0fcb9b905ef5754fb18a83e0380b3ae150e9
Sojka Michal <sojka@cngroup.dk>
2021-03-21 18:00:41 +0100

    Checkpoint 4 : Add Skills section markup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
+import { GetStaticProps } from 'next';
 
-import Hero from '../sections/Hero';
 import Layout from '../components/Layout';
+import Hero from '../sections/Hero';
+import Skills from '../sections/Skills';
+import { Skill } from '../types';
 
-export default function Home() {
+type Props = {
+  skills: Skill[];
+};
+
+export default function Home({ skills }: Props) {
   return (
     <Layout title="Kôlnička">
       <Hero />
+      <Skills skills={skills} />
     </Layout>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const skills = await require('../../data/skills.json');
+
+  return {
+    props: {
+      skills,
+    },
+  };
+};
