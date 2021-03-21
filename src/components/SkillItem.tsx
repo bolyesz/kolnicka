3c6d9729b749a4c435ce922c36394b1af9b0ded8
Sojka Michal <sojka@cngroup.dk>
2021-03-21 18:58:15 +0100

    Checkpoint 5 : Style skills section

--- src/components/SkillItem.tsx
+++ src/components/SkillItem.tsx
@@ -4,8 +4,22 @@ type Props = {
   skill: Skill;
 };
 
+const MAX_NO_ITEMS = 10;
+
 function SkillItem({ skill }: Props) {
-  return <li>{`${skill.name}-${skill.level}`}</li>;
+  return (
+    <li className="flex flex-col w-full py-4 lg:py-8 lg:pr-12 lg:w-1/3">
+      <h3 className="font-bold text-lg leading-10">{skill.name}</h3>
+      <div className="grid gap-x-0.5 sm:gap-x-1 grid-cols-10 w-full">
+        {[...Array(MAX_NO_ITEMS).keys()].map((index) => {
+          const isFilled = index < skill.level;
+          const color = isFilled ? 'bg-pink-500' : 'bg-gray-500';
+
+          return <div key={index} className={`${color} h-1`} />;
+        })}
+      </div>
+    </li>
+  );
 }
 
 export default SkillItem;
--- src/sections/Skills.tsx
+++ src/sections/Skills.tsx
@@ -9,9 +9,9 @@ type Props = {
 
 function Skills({ skills }: Props) {
   return (
-    <div>
-      <h2 className="text-4xl">My skillset</h2>
-      <ul>
+    <div className="my-16 p-8 border-pink-500 border-2">
+      <h2 className="text-4xl font-bold text-pink-500">My skillset</h2>
+      <ul className="mt-4 lg:flex lg:flex-wrap">
         {skills.map((skill) => (
           <SkillItem key={skill.name} skill={skill} />
         ))}
