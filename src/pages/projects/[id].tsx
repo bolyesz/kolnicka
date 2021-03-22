18d518f6b4fe59ba33ecdc5b8037f5d7b0eb3d17
Bólya Nikolas <bolya@cngroup.dk>
2021-03-22 20:07:56 +0100

    Checkpoint 10 : Add project detail page markup

--- src/pages/projects/[id].tsx
+++ src/pages/projects/[id].tsx
@@ -1,6 +1,12 @@
+import { format, parseISO } from 'date-fns';
 import { GetStaticPaths, GetStaticProps } from 'next';
+import React from 'react';
+import Image from 'next/image';
+
 import { getAllProjectIds, getProjectData } from '../../../lib/projects';
+import InfoBar from '../../components/InfoBar';
 import Layout from '../../components/Layout';
+import Subtitle from '../../components/Subtitle';
 import { Project } from '../../types';
 
 type Props = {
@@ -9,8 +15,36 @@ type Props = {
 
 function ProjectDetail({ projectData }: Props) {
   return (
-    <Layout title="Title">
-      <div className="h-screen">{`Hi ${projectData.position}`}</div>
+    <Layout title={`Project | ${projectData.name}`}>
+      <Subtitle subtitle={projectData.name} />
+
+      <div>
+        <span>{format(parseISO(projectData.timeStart), 'dd. MMMM yyyy')}</span>
+        <span> - </span>
+        <span>{format(parseISO(projectData.timeEnd), 'dd. MMMM yyyy')}</span>
+        <span> @ {projectData.company}</span>
+      </div>
+      <span>{projectData.position}</span>
+      <p>{projectData.description}</p>
+
+      <h3>Used technologies</h3>
+      <InfoBar
+        infos={projectData.technologies.map((technology) => ({
+          label: technology,
+        }))}
+      />
+      <div className="flex">
+        {projectData.images.map((image, index) => (
+          <div key={index} className="relative w-96 h-96 mr-2">
+            <Image
+              src={image}
+              alt="Project motto"
+              layout="fill"
+              objectFit="contain"
+            />
+          </div>
+        ))}
+      </div>
     </Layout>
   );
 }
